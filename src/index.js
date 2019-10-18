96f06da64701ecea6444980fcc618bec100dc125
ted_scanlan <t.scanlan92@gmail.com>
2019-10-18 18:29:51 +0100

    imported browser router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));   //renders the app into the element in index.html with id of 'root'
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
