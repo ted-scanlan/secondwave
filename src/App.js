b63ef8cc385a992aeda3a012227af2f56094729b
ted_scanlan <t.scanlan92@gmail.com>
2019-10-19 00:27:51 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import Navbar1 from "./components/navbar.component";
+import BoardsList from "./components/boards-list.component";
+import CreateBoard from "./components/create-board.component";
+import CreateUser from "./components/create-user.component";
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <Router>
+    <Navbar1 />
+    <br/>
+    <Route path="/" exact component={BoardsList} />
+    <Route path="/create" exact component={CreateBoard} />
+    <Route path="/user" exact component={CreateUser} />
+
+    </Router>
   );
 }
 
---
+++ src/components/navbar.component.js
@@ -0,0 +1,31 @@
+import React, { Component, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+
+const Navbar1 = (props) => {         // this is a function component rather than the normal class component, as this allows us to use hooks. this function could also be written as: function Example(props) {}. these were originally stateless components but we can not "hook" into React feautures such as state . useState is a Hook that lets you add React state to a function compoenent. https://reactjs.org/docs/hooks-state.html
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">SecondWave</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/components/">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Search for a board</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navbar1;
